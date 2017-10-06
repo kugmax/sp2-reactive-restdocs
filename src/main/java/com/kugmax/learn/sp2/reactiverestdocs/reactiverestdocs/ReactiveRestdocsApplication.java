cbd302be924f18de604fb7ffe1a09c10420bc550
maxim.kugatov <maxim.kugatov@digitalhouse.biz>
2017-10-06 13:57:54 +0300

    initial

---
+++ src/main/java/com/kugmax/learn/sp2/reactiverestdocs/reactiverestdocs/ReactiveRestdocsApplication.java
@@ -0,0 +1,12 @@
+package com.kugmax.learn.sp2.reactiverestdocs.reactiverestdocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveRestdocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveRestdocsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kugmax/learn/sp2/reactiverestdocs/reactiverestdocs/ReactiveRestdocsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kugmax.learn.sp2.reactiverestdocs.reactiverestdocs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReactiveRestdocsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
